7409c510ab71c90471bca757fde0f312237e5c0b
kumarsai08 <kumarsai0802@gmail.com>
2023-04-26 12:30:16 +0530

    Lwc wedenesday git push

--- force-app/main/default/lwc/pF_Place_Order_Tab/pF_Place_Order_Tab.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement,api } from 'lwc';
-
-export default class PF_Place_Order_Tab extends LightningElement {
-    @api selectedRowOnly;
-    connectedCallback(){
-        console.log('selectedRowOnly:'+this.selectedRowOnly);
-    }
-}
